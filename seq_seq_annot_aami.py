5f6c1053c800b60be349d3fb120596498e070a04
ikaya <ibrahimkaya754@gmail.com>
2019-10-04 17:14:00 +0300

    parser were changed with args_dictionary

--- seq_seq_annot_aami.py
+++ seq_seq_annot_aami.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7519e937d388cb4c409f9265b0ee006bbe5bc275ee2a2f5112becef303c1a419
-size 19837
+oid sha256:e7790bd2f2277a17fbe6dfe106fc0ffa20e89261e16cef54ca53fef6c3caf15c
+size 21420
