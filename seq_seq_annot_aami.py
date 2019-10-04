b46cbaaee9ee73e1efd4b96c84d17ce70188e7ee
ikaya <ikaya754@gmail.com>
2019-10-04 17:04:10 +0300

    arguments were changed with dictionary

--- seq_seq_annot_aami.py
+++ seq_seq_annot_aami.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7519e937d388cb4c409f9265b0ee006bbe5bc275ee2a2f5112becef303c1a419
-size 19837
+oid sha256:4c6b17d79fc414362ca1a9a272dc803ebe41b46974b542f5b3dd7751914a51a5
+size 21337
